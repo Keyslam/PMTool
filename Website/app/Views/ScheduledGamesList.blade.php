37dba4086c0759e2e328c5ec4e080b5dfbf8ad16
Patrick Riensta <prive@p-rienstra.nl>
2020-04-10 19:13:27 +0200

    Added  new Game Option
    Added the posibility to schedule new games
    Started the code for changing game settings

--- Website/app/Views/ScheduledGamesList.blade.php
+++ Website/app/Views/ScheduledGamesList.blade.php
@@ -1,5 +1,12 @@
 <ul class="collection">
-@foreach ($scheduledGames as $scheduledGame)
-	<li class="collection-item"><b>Test Spel {{ $scheduledGame["ID"] }}</b><br>{{ $scheduledGame["StartTime"] }}</li>
-@endforeach
+    @if(count($scheduledGames) > 0)
+        @foreach ($scheduledGames as $scheduledGame)
+            <li class="collection-item games">
+                <b>{{ $scheduledGame["StartTime"] }}</b>
+                <input type="text" value="{{$scheduledGame["ID"]}}" hidden>
+            </li>
+        @endforeach
+    @else
+        <li class="collection-item"><b>Er zijn geen geplande games</b></li>
+    @endif
 </ul>
