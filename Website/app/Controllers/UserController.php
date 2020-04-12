9cd9f071fbcd8fbe70385b1d1517b58aae7788cb
Tjakka5 <justinvanderleij@gmail.com>
2020-04-12 16:02:54 +0200

    Fix division to 0

--- Website/app/Controllers/UserController.php
+++ Website/app/Controllers/UserController.php
@@ -1,36 +1,39 @@
 <?php
 class UserController {
 	public function indexAction() {
-        Middleware::getMethod();
-        Middleware::IsUser();
+		Middleware::getMethod();
+		Middleware::IsUser();
 
 		echo blade()->run("Home");
-    }
-    
-    public function signupAction() {
-        Middleware::getMethod();
-        Middleware::IsUser();
+	}
+	
+	public function signupAction() {
+		Middleware::getMethod();
+		Middleware::IsUser();
 
-        echo blade()->run("GameSignup");
-    }
+		echo blade()->run("GameSignup");
+	}
 
-    public function statisticsAction() {
-        Middleware::getMethod();
-        Middleware::IsUser();
+	public function statisticsAction() {
+		Middleware::getMethod();
+		Middleware::IsUser();
 
-        //echo blade()->run("UserStatistics"); // TODO
-
-        try {
-            $stmt = DB::Connection()->prepare("SELECT AmountWon, HandsWon, HandsPlayed FROM GameStatistics WHERE UserID = :UserID");
-            $stmt->bindValue("UserID", $_SESSION["user_id"]);
+		try {
+			$stmt = DB::Connection()->prepare("SELECT AmountWon, HandsWon, HandsPlayed FROM GameStatistics WHERE UserID = :UserID");
+			$stmt->bindValue("UserID", $_SESSION["user_id"]);
 			if ($stmt->execute()) {
 				$results = $stmt->fetch();
+				
+				$averageHandsWon = 0;
+				if ($results["HandsPlayed"] > 0) {
+					$averageHandsWon = round($results["HandsWon"]/$results["HandsPlayed"] * 100);
+				}
 
 				echo blade()->run("UserStatistics", [
-                    "amountWon" => $results["AmountWon"],
-                    "handsWon" => $results["HandsWon"],
-                    "handsPlayed" => $results["HandsPlayed"],
-                    "averageHandsWon" => round($results["HandsWon"]/$results["HandsPlayed"] * 100)
+					"amountWon" => $results["AmountWon"],
+					"handsWon" => $results["HandsWon"],
+					"handsPlayed" => $results["HandsPlayed"],
+					"averageHandsWon" => $averageHandsWon,
 				]);
 			} else {
 				Redirect::internalServerError();
@@ -38,16 +41,16 @@ class UserController {
 		} catch (Exception $exception) {
 			Redirect::internalServerError();
 		}
-    }
+	}
 
-    public function logoutGETAction() {
-        Middleware::getMethod();
-        Middleware::IsUser();
+	public function logoutGETAction() {
+		Middleware::getMethod();
+		Middleware::IsUser();
 
-        unset($_SESSION["user_id"]);
-        unset($_SESSION["user_is_admin"]);
+		unset($_SESSION["user_id"]);
+		unset($_SESSION["user_is_admin"]);
 
-        Redirect::home();
-    }
+		Redirect::home();
+	}
 }
 ?>
--- Website/app/Views/UserStatistics.blade.php
+++ Website/app/Views/UserStatistics.blade.php
@@ -3,8 +3,8 @@
 @section("title", "UserStatistics")
 
 @section("constrained-content")
-    <h1>Gewonnen games: {{ $amountWon}}</h1>
-    <h1>Handen gewonnen: {{ $handsWon}}<h1>
-    <h1>Handen gespeeld: {{ $handsPlayed}}</h1>
-    <h1>Gemiddelde gewonnen handen: {{$averageHandsWon}}%</h1>
+	<h1>Gewonnen games: {{ $amountWon}}</h1>
+	<h1>Handen gewonnen: {{ $handsWon}}<h1>
+	<h1>Handen gespeeld: {{ $handsPlayed}}</h1>
+	<h1>Gemiddelde gewonnen handen: {{$averageHandsWon}}%</h1>
 @endsection
